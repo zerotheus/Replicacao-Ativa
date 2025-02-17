6762a62acd9813447fd3f084106db9859deeb59c
Ntheus <matheusoliveira.7313@gmail.com>
2025-02-17 02:28:31 -0300

    dando um id a cada comando

--- src/main/java/sistemas/distribuidos/replicacaoativa/model/SQLCommand.java
+++ src/main/java/sistemas/distribuidos/replicacaoativa/model/SQLCommand.java
@@ -1,8 +1,11 @@
 package sistemas.distribuidos.replicacaoativa.model;
 
+import java.util.UUID;
+
 import lombok.Data;
 
 @Data
 public class SQLCommand {
+    private UUID id = UUID.randomUUID();
     private String sql;
 }
